3c577d30de1d970b4213186af418cc40abb0adf0
liuwei <levonfly@gmail.com>
2021-01-07 16:07:04 +0800

    add domian

---
+++ yuming/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	name := os.Args[1]
+	fi, err := os.Open(name)
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+		return
+	}
+	defer fi.Close()
+
+	br := bufio.NewReader(fi)
+	head := uint8('a')
+	count := 0
+	for {
+		a, _, c := br.ReadLine()
+		if c == io.EOF {
+			break
+		}
+		str := string(a)
+		str = strings.ToLower(str)
+		str = str[0:len(str)-4]
+
+
+		max := 0
+		maxA := ""
+		for i := 'a'; i <= 'z'; i++ {
+			a := string(i)
+			n := strings.Count(str, a)
+			if max < n {
+				max = n
+				maxA = a
+			}
+		}
+		if max > 3{
+
+
+			if head == str[0] {
+				count++
+			}else{
+				fmt.Println("--------------------------",string(head),count)
+				head = str[0]
+				count=1
+			}
+
+
+			fmt.Println(str,max,maxA)
+		}
+	}
+}
