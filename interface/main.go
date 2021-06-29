62daa976cd3867fa0a38474dbc4259bd518f2d10
liuwei <levonfly@gmail.com>
2021-06-29 19:19:09 +0800

    添加

--- interface/main.go
+++ interface/main.go
@@ -11,18 +11,32 @@ type Info struct {
 	C string `json:"c"`
 }
 
+type AA struct {
+	*Info
+	string
+}
+
 func main() {
 	info := &Info{A: "str1", B: 100, C: "str2"}
 
+	// 这里来了两个测试的动态结构
 	vars1 := `{"d":"123"}`
-	vars2 := `{"e":"123", "f":200}`
-	outPuts := make(map[string]interface{})
-	respByte, _ := json.Marshal(info)
-	_ = json.Unmarshal(respByte, &outPuts)
-	_ = json.Unmarshal([]byte(vars1), &outPuts)
-	_ = json.Unmarshal([]byte(vars2), &outPuts)
+	//vars2 := `{"e":"123", "f":200}`
 
-	outputJson(outPuts)
+	//outPuts := make(map[string]interface{})
+	//
+	//respByte, _ := json.Marshal(info)
+	//_ = json.Unmarshal(respByte, &outPuts)
+	//
+	//_ = json.Unmarshal([]byte(vars1), &outPuts)
+	//_ = json.Unmarshal([]byte(vars2), &outPuts)
+
+	o := struct {
+		*Info
+		interface{}
+	}{info, vars1}
+
+	outputJson(o)
 }
 
 // 理解成给前端的json
