7661915131b3bef0f1a83d666148eb63b280aef7
unix2dos <levonfly@gmail.com>
2024-03-20 20:43:15 +0800

    增加log

--- go/main.go
+++ go/main.go
@@ -1,51 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+)
 
 func main() {
-	arr := []int{21, 2, 1, 60, 32}
-	Print(arr) //21      2       1       60      32
-	QuickSort(arr, 0, len(arr)-1)
-	Print(arr) //1       2       21      32      60
+	fmt.Println(deleteKey("user::cp_bell_num::2023-09-25::241445913631"))
+	//fmt.Println(deleteKey(""))
 }
 
-func QuickSort(arr []int, left, right int) {
-	if left < right {
-		mid := partition(arr, left, right)
-		QuickSort(arr, left, mid-1)
-		QuickSort(arr, mid+1, right)
-	}
+func createClient() *redis.Client {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "r-2ze1c8t4jhzsgsahzx.redis.rds.aliyuncs.com:6379", // Redis 地址
+		Password: "t9gwDg5zL0",                                       // Redis 密码，如果没有设置密码，可以为空字符串 ""
+		DB:       1,                                                  // Redis 数据库索引
+	})
+
+	return client
 }
 
-func partition(arr []int, left, right int) int {
+func deleteKey(key string) error {
+	client := createClient()
 
-	value := arr[left] // 两人心中所想的数字
-	start := left      // 记录所想数字的位置，相遇了要交换
+	// 使用 Del 方法删除键
+	result := client.Del(context.Background(), key)
 
-	for left != right { // 两人不相遇，就循环
-
-		for left < right && arr[right] >= value { //牛郎先走，大于等于就走，小于就停
-			right--
-		}
-		for left < right && arr[left] <= value { //织女后走，小于等于就走，大于就停
-			left++
-		}
-
-		if left < right { //双方停止后打电话交换脚下的数据
-			arr[left], arr[right] = arr[right], arr[left]
-		}
-		//接着循环，直到两人相遇
+	if err := result.Err(); err != nil {
+		return err
 	}
 
-	// 相遇了，把两人脚下数据和心中数字交换，下面 left 和 right 是相等的
-	arr[start], arr[left] = arr[left], arr[start]
-
-	return left
-}
-
-func Print(arr []int) {
-	for _, v := range arr {
-		fmt.Printf("%d\t", v)
-	}
-	fmt.Print("\n")
+	return nil
 }
---
+++ jinquan_scripts/mongo.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	// 是否测试环境
+	isTest bool = true
+
+	// 测试环境
+	TestMongoLink = "mongodb://poros_c:PKWyKN512EbNswf4XNBjXehDM2cNjxwR@s-2ze804aac801b914-pub.mongodb.rds.aliyuncs.com:3717/jinquan"
+	TestDB        = "jinquan"
+
+	// !!!正式环境!!!
+	ProdMongoLink = "mongodb://user-core-service:0533e9234fa25a7f70d5a9cc29415488@s-2ze1de83b7e68e94.mongodb.rds.aliyuncs.com:3717,s-2ze0700f4cd183a4.mongodb.rds.aliyuncs.com:3717,s-2ze1a63a895afc84.mongodb.rds.aliyuncs.com:3717,s-2ze61d1c3f83a114.mongodb.rds.aliyuncs.com:3717,s-2zec3e106b3b6e94.mongodb.rds.aliyuncs.com:3717,s-2zedac4025f24254.mongodb.rds.aliyuncs.com:3717/poros"
+	ProdDB        = "poros"
+)
+
+func main() {
+	client := getClient()
+	GetUser(client)
+}
+
+func GetUser(client *mongo.Client) {
+	mongoDB := TestDB
+	if !isTest {
+		mongoDB = ProdDB
+	}
+	conn := client.Database(mongoDB).Collection("user")
+	var result bson.M
+	filter := bson.M{"_id": "1572211991"}
+	err := conn.FindOne(context.Background(), filter).Decode(&result)
+	a, _ := json.Marshal(result)
+	fmt.Println("res:", string(a))
+	fmt.Println("err:", err)
+}
+
+func getClient() *mongo.Client {
+	mongoLink := TestMongoLink
+	if !isTest {
+		mongoLink = ProdMongoLink
+	}
+	// Set client options
+	clientOptions := options.Client().ApplyURI(mongoLink)
+	// Connect to MongoDB
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return client
+}
