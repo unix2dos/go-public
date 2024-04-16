9f8a6486043804ee0ada8bf2d55686a4ab726ecc
unix2dos <levonfly@gmail.com>
2024-04-16 13:56:19 +0800

    update test

--- main.go
+++ main.go
@@ -1,21 +1,10 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
 func main() {
-	fmt.Println(ReverseWord("never too later to learn"))
+	var arr = []int{10, 99, 73, 29, 7, 3, 3, 100}
+
 }
 
-// 输入： never too later to learn
-// 输出： learn to later too never
+func QuickSort(arr []int, left int, right int) {
 
-func ReverseWord(str string) string {
-	arr := strings.Fields(str)
-	for i := 0; i < len(arr)/2; i++ {
-		arr[i], arr[len(arr)-1-i] = arr[len(arr)-1-i], arr[i]
-	}
-	return strings.Join(arr, " ")
 }
