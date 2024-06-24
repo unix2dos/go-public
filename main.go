35f6d5d6594ac23698b3b7f4b2dfcfd75f077ad4
liuwei <levonfly@gmail.com>
2024-06-24 10:23:32 +0800

    update test

--- main.go
+++ main.go
@@ -1,21 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
 func main() {
-	fmt.Println(ReverseWord("never too later to learn"))
-}
-
-// 输入： never too later to learn
-// 输出： learn to later too never
-
-func ReverseWord(str string) string {
-	arr := strings.Fields(str)
-	for i := 0; i < len(arr)/2; i++ {
-		arr[i], arr[len(arr)-1-i] = arr[len(arr)-1-i], arr[i]
-	}
-	return strings.Join(arr, " ")
 }
